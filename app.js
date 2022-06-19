0209b0776b7091ab01469b563e0fa835557c0d76
Maggie Chang <maggiechang@maggies-mbp.dyn.brandeis.edu>
2022-06-19 11:56:22 -0400

    new changes on ghibli

--- app.js
+++ app.js
@@ -97,19 +97,20 @@ app.get('/ghibli',(req,res,next) => {
   res.render('ghibli')
 })
 
-app.get('/studioghibli',(req,res,next) => {
-  res.render('studioghibli')
-})
-
 app.post('/ghibli',
   async (req,res,next) => {
-    const id = req.body.id;
-    const url="https://ghibliapi.herokuapp.com/films/"+id
-    const response = await axios.get(url)
-    console.dir(response.data)
-    res.locals.id = id
-    res.locals.id = response.data.id
-    res.render('showGhibli')
+    try{
+      const movie = req.body.movie;
+      const url="https://ghibliapi.herokuapp.com/films/"+movie
+      const response = await axios.get(url)
+      console.dir(response.data)
+      res.locals.movie = response.data
+      res.render('showGhibli')
+    }
+    catch (err){
+      console.error("Something went wrong")
+      console.error(err)      
+    }
   })
 
 
